370ab5fe171bfc3916fe2e6cc7fbce021838d242
ssenr <duranr@protonmail.com>
2023-02-19 20:38:46 -0500

    Fixed Display Script

--- .screenlayout/disp.sh
+++ .screenlayout/disp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --off
+xrandr --output HDMI-0 --mode 1920x1080 --rate 144 --output DP-4 --mode 1920x1080 --rate 239.76 --output DVI-D-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-5 --off
