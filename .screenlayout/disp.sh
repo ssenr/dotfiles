87219fa66c3b9aa170952b3c64da195f6e97bc93
ssenr <duranr@protonmail.com>
2023-03-10 13:23:59 -0500

    Fixed disp.sh script

--- .screenlayout/disp.sh
+++ .screenlayout/disp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --rate 144 --output DP-4 --mode 1920x1080 --rate 239.76 --output DVI-D-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-5 --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --rate 144 --output DP-4 --mode 1920x1080 --rate 239.76 --rotate normal --pos 1920x0 --primary --output DVI-D-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-5 --off
